532161ed10d7de724101cd675ef89a6504bcbb7b
Rajkumar <rajkumaar2304@gmail.com>
2019-04-11 16:08:12 +0530

    Completes faculty timetable
    Signed-off-by: Rajkumar <rajkumaar2304@gmail.com>

--- amritarepo/js/faculty-timetable.js
+++ amritarepo/js/faculty-timetable.js
@@ -11,18 +11,25 @@ $(document).ready(function(){
     years.append('<option value="' + (currentYear) + "_" + (currentYear +1).toString().substring(2, 4) + '">' + (currentYear) + "_" + (currentYear+1).toString().substring(2, 4) +  '</option>');
     years.append('<option value="' + (currentYear + 1) + "_" + (currentYear +2).toString().substring(2, 4) + '">' + (currentYear + 1) + "_" + (currentYear+2).toString().substring(2, 4) +  '</option>');
 
+    $('#year option').eq(getCookie("fac-year")).prop('selected', true);
+    $('#sem option').eq(getCookie("fac-sem")).prop('selected', true);
     let result = [];
     $('#faculty').on('input', function() {
         $.ajax({
-            method: 'POST',
-            url: "https://dev.rajkumaar.co.in/proxyhey.php",
-            data : {
-              'data' :   "https://intranet.cb.amrita.edu/TimeTable/Faculty/get_staff_list.php?q="+$('#faculty').val()
-            },
+            method: 'GET',
+            url: "https://intranet.cb.amrita.edu/TimeTable/Faculty/get_staff_list.php?q="+$('#faculty').val(),
             success: (res) => {
                 result = res.split('\n');
-                $("#faculty").autocomplete({
-                    source: result
+                $("#faculty").typeahead({
+                    source: function (request, response) {
+                        response(result);
+                        $(".dropdown-menu").css("width", "auto");
+                        $(".dropdown-menu").css("height", "auto");
+                        $(".dropdown-menu").css("font", "15px Varela Round");
+                    },
+                    hint: true,             // SHOW HINT (DEFAULT IS "true").
+                    highlight: true,        // HIGHLIGHT (SET <strong> or <b> BOLD). DEFAULT IS "true".
+                    minLength: 1            // MINIMUM 1 CHARACTER TO START WITH.
                 });
             }
         });
@@ -32,28 +39,52 @@ $(document).ready(function(){
 });
 
 function viewOrDownload() {
+    let year = ($('#year').children("option:selected"));
+    let sem = ($('#sem').children("option:selected"));
+
     let data = {
-        "year": $('#year').val(),
-        "Nyear":$('#year').val(),
-        "sem":$('#sem').val(),
-        "Nsem":$('#sem').val(),
+        "year": year.val(),
+        "Nyear":year.val(),
+        "sem":sem.val(),
+        "Nsem":sem.val(),
         "NAMEshwbutton": "Show Details",
         "faculty":$('#faculty').val()
     };
+    setCookie("fac-year",year.index());
+    setCookie("fac-sem",sem.index());
     $.ajax({
-        method: 'POST',
+        type: 'POST',
         url:"https://intranet.cb.amrita.edu/TimeTable/Faculty/index.php",
         data: data,
-        success : (res) => {
-         console.log(res.findByElement('iframe'));
+    success : (res) => {
+            let url = res.toString().split("<iframe")[1].split("src=\"")[1].split("\"")[0];
+        let win = window.open("https://intranet.cb.amrita.edu/TimeTable/Faculty/"+url, '_blank');
+        win.focus();
         }
     });
 }
 
-function fillNames(names) {
-    let nameSelect =$('#faculty');
-    nameSelect.empty();
-    for (let i=0 ;i<names.length;++i){
-        nameSelect.append($("<option></option>").text(names[i]));
+//Sets a cookie on key=>value
+function setCookie(key,value) {
+    let d = new Date();
+    d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = key + "=" + value + ";" + expires + ";path=/";
+}
+
+
+//Gets cookie from key
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
     }
+    return "";
 }

7cdb85cd73980a3a0ed986f248858daa21014d6d
Rajkumar S <rajkumaar2304@icloud.com>
2020-07-03 11:36:18 +0530

    Moves web app to a separate repo
    Signed-off-by: Rajkumar S <rajkumaar2304@icloud.com>

--- static/amritarepo/js/aums-home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    if (localStorage.getItem("token") == null) {
-        window.location.href = 'index.html';
-    }
-    let greeting = $('#greeting');
-    greeting.html("Welcome " + localStorage.getItem("name"));
-});
